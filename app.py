f05603e42b772e996c81f8ebef360c92a85b7291
Alvaro-Ciudad <93728906+Alvaro-Ciudad@users.noreply.github.com>
2022-01-29 21:07:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PotatoPatato'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'lalala'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'lalala'  # Falta
 Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'loging'
 
 
 class LogingForm(FlaskForm):
@@ -26,28 +32,46 @@ class RegistrationForm(FlaskForm):
                                                                              max=80)])
 
 
-class User(db.Model):
+class User(UserMixin, db.Model): #quizas hace falta unique id pa esto, tengo duda
+    __tablename__ = 'User'
     username = db.Column(db.String(25), primary_key=True, unique=True)
     password = db.Column(db.String(80))
 
 
+@login_manager.user_loader
+def load_user(username):
+    return User.query.get(str(username))
+
+
 class Analysis(db.Model):
+    __tablename__ = 'Analysis'
     id = db.Column(db.Integer, primary_key=True)
     Ampreg = db.Column(db.Boolean)
     Hydro = db.Column(db.Boolean)
     Isopoint = db.Column(db.Boolean)
     Blastp = db.Column(db.Boolean)
     Date = db.Column(db.DateTime)
+    Error = db.Column(db.String(80))
+    user_id = db.Column(db.String(25), db.ForeignKey("User.username"))
+    table_id = db.Column(db.Integer, db.ForeignKey("Table.id"))
+    user = db.relationship("User", foreign_keys=[user_id])
+    table = db.relationship("Table", foreign_keys=[table_id])
 
 
 class Files(db.Model):
+    __tablename__ = 'Files'
     id = db.Column(db.Integer, primary_key=True)
     impout = db.Column(db.Boolean)
     path = db.Column(db.String(80))
     queryid = db.Column(db.Integer)
+    user_id = db.Column(db.String(25), db.ForeignKey("User.username"))
+    analysis_id = db.Column(db.Integer, db.ForeignKey("Analysis.id"))
+    user = db.relationship("User", foreign_keys=[user_id])
+    analysis = db.relationship("Table", foreign_keys=[analysis_id])
 
 
 class Table(db.Model):
+    __tablename__ = 'Table'
     id = db.Column(db.Integer, primary_key=True)
     typetable = db.Column(db.Integer)
 
@@ -60,13 +84,33 @@ def index():
 @app.route('/loging', methods=['GET', 'POST'])
 def loging():
     form = LogingForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                #login_user(user, remember=True) esto es lo del dashboard, pruebalo si quieres el remember quizas hay que quitarlo
+                return redirect(url_for('workspace'))
+            return '<h1>Invalid username or password</h1>'
     return render_template('loging.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data,
+                        password=hashed)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>Works yey</h1>'
     return render_template('register.html', form=form)
+
+
+@app.route('/workspace')
+@login_required
+def dashboard():
+    return render_template('workspace.html')
 
 
 if __name__ == '__main__':
